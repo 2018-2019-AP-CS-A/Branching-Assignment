98ade3ff66fa9ca4c9dfa3d0aedd03d2fda123f4
120mperkins <51206453+120mperkins@users.noreply.github.com>
2019-06-10 12:48:55 -0400

    Not done yet

---
+++ Branching-Assignment/src/Perkins_Matthew/Exercise_Driver.java
@@ -0,0 +1,10 @@
+package Perkins_Matthew;
+
+public class Exercise_Driver 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(Exercises.threeAndFive());
+		System.out.println(Exercises.evenFibSum());
+	}
+}
