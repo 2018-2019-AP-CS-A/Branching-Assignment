fbfd7cf88264392aff535a487462a92366033f66
rgiova27 <43549268+rgiova27@users.noreply.github.com>
2019-06-10 12:54:45 -0400

    started

---
+++ Branching-Assignment/src/Giovanniello_Ryan/Driver.java
@@ -0,0 +1,16 @@
+package Giovanniello_Ryan;
+
+/**
+ * @author Ryan Giovanniello
+ */
+public class Driver
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println(Exercises.threeAndFive());
+		System.out.println(Exercises.evenFibSum());
+		System.out.println(Exercises.largestPrimeFactor());
+	}
+
+}
