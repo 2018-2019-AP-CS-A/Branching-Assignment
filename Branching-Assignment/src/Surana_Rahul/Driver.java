2609ec2bf2d170d2a7f6a4b3fc7154f37c3051ba
RahulSurana <34356101+RahulSurana1@users.noreply.github.com>
2019-06-10 12:54:32 -0400

    finsihed

---
+++ Branching-Assignment/src/Surana_Rahul/Driver.java
@@ -0,0 +1,14 @@
+package Surana_Rahul;
+
+public class Driver 
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Exercises.threeAndFive());
+		System.out.println(Exercises.evenFibSum());
+		System.out.println(Exercises.largestPrimeFactor());
+
+
+		
+	}
+}
