def5c81bb4b544c43a2c658bbdc17eeedef9ff53
hiimharry <45776127+hiimharry@users.noreply.github.com>
2019-06-10 12:40:18 -0400

    I filled in the Methods
    :)

---
+++ Branching-Assignment/src/Allex_Harry/Driver.java
@@ -0,0 +1,12 @@
+/**
+ * @author Harry Allex
+ */
+package Allex_Harry;
+
+public class Driver 
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Exercises.threeAndFive() + " " + Exercises.evenFibSum() + " " +  Exercises.largestPrimeFactor());
+	}
+}
