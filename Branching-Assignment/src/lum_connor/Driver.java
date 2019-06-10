06c694db77c37635872ce17c5c45a67725e2e0a9
connorl36 <51222904+connorl36@users.noreply.github.com>
2019-06-10 12:41:42 -0400

    Completed Assignment

---
+++ Branching-Assignment/src/lum_connor/Driver.java
@@ -0,0 +1,15 @@
+/**
+ * @author Connor Lum
+ */
+package lum_connor;
+
+public class Driver 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(Exercises.threeAndFive());
+		System.out.println(Exercises.evenFibSum());
+		System.out.println(Exercises.largestPrimeFactor());
+		
+	}
+}
