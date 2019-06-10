a05c50463236a37f20a03570cc601c4035e87a48
Garrett Schoenfelder <120gschoenfelder@frhsd.com>
2019-06-10 12:21:02 -0400

    Changed header to fit properly

--- Branching-Assignment/src/Schoenfelder_Garrett/Exercises.java
+++ Branching-Assignment/src/Schoenfelder_Garrett/Exercises.java
@@ -1,29 +1,6 @@
 /**
  * @author Garrett Schoenfelder
  * 
- * Assignment Instructions:
- * 	- Right click on the src folder and create a new package.
- * 	- valueame the package "lastvalueame_firstvalueame", replacing lastvalueame with your last name and firstvalueame with your first name.
- * 	- Inside that package, create a new class "Exercises".
- *    - YOU SHOULD OvalueLY BE MAKIvalueG CHAvalueGES Ivalue THIS PACKAGE
- * 	- Create a javadoc comment at the top with the '@author' tag with your name next to it (do this for all classes you create)
- * 	- Create the following static methods:
- * 		- int threeAndFive() : Returns the sum of all the multiples of 3 or 5 below 1000
- * 		- int evenFibSum() : Returns the sum of the even-valued terms less than 4 million in the Fibonacci Sequence
- * 		- int largestPrimeFactor() : Returns the largest prime factor of the number 600851475143
- * 	- Create a separate Driver class and test your program by outputting the answers to your static methods
- *
- * Submission Instructions:
- *  - Save your saved work on Eclipse - your workspace should be the file location of the cloned repo
- *  - In GitHub Desktop, add your summary and commit your changes
- *  - Then push your commits to GitHub
- *  - Go to your repo on GitHub.com
- *  - Click "valueew pull request"
- *  - Click "Create pull request"
- *  - In the summary for your request, put your first and last name
- *  - Send the request
- * 
- * 
  */
 
 package Schoenfelder_Garrett;
