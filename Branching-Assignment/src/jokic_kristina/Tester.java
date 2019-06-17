a3c5133dacc08f6e647c1c6baac741a88e29f09c
achiu-frhsd <41763388+achiu-frhsd@users.noreply.github.com>
2019-06-17 15:47:25 -0400

    Testers added

---
+++ Branching-Assignment/src/jokic_kristina/Tester.java
@@ -0,0 +1,20 @@
+package jokic_kristina;
+
+public class Tester
+{
+	public static void main(String[] args)
+	{
+		if(Exercises.threeAndFive() == 233168)
+			System.out.println("Correct");
+		else
+			System.out.println("Incorrect");
+		if(Exercises.evenFibSum() == 4613732)
+			System.out.println("Correct");
+		else
+			System.out.println("Incorrect");
+		if(Exercises.largestPrimeFactor() == 6857)
+			System.out.println("Correct");
+		else
+			System.out.println("Incorrect");
+	}
+}
